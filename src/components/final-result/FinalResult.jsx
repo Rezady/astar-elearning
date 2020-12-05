a25647889e222544728f8c9d4d7a0477f9b2b4da
Rezady <rezady30@gmail.com>
2020-12-05 15:54:43 +0700

    menambahkan UI final result

--- src/components/final-result/FinalResult.jsx
+++ src/components/final-result/FinalResult.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 const FinalResult = (props) => {
     return (
 
-
         <div div class="card mx-auto align-middle border-light d-flex" id="body-question-1" >
 
             <div class="card-body">
@@ -11,6 +10,7 @@ const FinalResult = (props) => {
                 <h4 class="card-title text-center font-weight-bold ">benar : {props.result / 10}</h4>
                 <h4 class="card-title text-center font-weight-bold ">salah : {(props.result / 10) - 8}</h4>
                 <h3 class="card-title text-center font-weight-bold ">Apakah Anda Ingin bermain Lagi ?  </h3>
+
             </div>
         </div >
     )
